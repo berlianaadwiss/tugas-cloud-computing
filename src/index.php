14d1159fd7d55b31bfebb7e432e3d91f50045fcd
berlianaadwiss <berlianadwis10@gmail.com>
2025-10-29 07:19:14 +0700

    update index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,8 @@
-<?php
-echo "<h1>Hello I'm Berliana, CI/CD PHP from GitHub!</h1>";
-echo "<p>Pipeline berhasil dijalankan 🚀</p>";
-?>
+<?php 
+echo "<h1>
+ 🚀
+ Hello, I'm Berliana! I hope this code PHP CI/CD Pipeline is 
+running successfully!</h1>"; 
+echo "<p>Build and deployed automatically via GitHub Actions.</p>"; 
+echo "<p>Image source: dockerhub_berlinberlin/php-demo:latest</p>"; 
+?> 
